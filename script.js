f2e61aee5e4d8038d859c2682ae714ded22552c8
Trasiu <kuba6356@gmail.com>
2023-03-20 23:27:17 +0100

    fixed a lot of bugs made the font a little bigger and added unbeatable ai

--- script.js
+++ script.js
@@ -47,23 +47,79 @@ function makegame() {
       const block = document.createElement("div");
       block.classList.add("block");
       block.setAttribute("id", `${x}, ${y}`);
+      let z = 0
       document.getElementById("row" + x).appendChild(block);
+      if (x == 2 && y == 2) {
+        z = 1
+      }
       let square = {
         row: x,
         block: y,
         takenBy: "",
         id: document.getElementById(`${x}, ${y}`),
+        value: z,
       };
       board.push(square);
     }
   }
   let winner = false;
   let counter = 0;
+  console.log(board)
   board.forEach((key) => {
     key.id.addEventListener("click", () => {
       if (key.takenBy == "") {
         key.id.textContent = player;
         key.takenBy = player;
+        for (let x = 0; x <= 8; x++) {
+          if (key.row == board[x].row || key.block == board[x].block) {
+            board[x].value++
+          }
+          else if ((key == board[0]) && (board[4].takenBy == `${player}`) || (key == board[8]) && (board[4].takenBy == `${player}`)) {
+            board[0].value = 9
+            board[4].value = 9
+            board[8].value = 9
+          }
+          else if ((key == board[2]) && (board[4].takenBy == `${player}`) || (key == board[6]) && (board[4].takenBy == `${player}`)) {
+            board[2].value = 9
+            board[4].value = 9
+            board[6].value = 9
+          }
+          if ((key.row == board[x].row) && (key != board[x])) {
+            if (key.takenBy === board[x].takenBy) {
+              console.log(board[x])
+              console.log(key)
+              for (let z = 0; z <= 8; z++) {
+                if (board[z].row == key.row) {
+                  board[z].value = 9
+                }
+
+              }
+            }
+          }
+          if ((key.block == board[x].block) && (key != board[x])) {
+            if (key.takenBy === board[x].takenBy) {
+              console.log(board[x])
+              console.log(key)
+              for (let z = 0; z <= 8; z++) {
+                if (board[z].block == key.block) {
+                  board[z].value = 9
+                }
+
+              }
+            }
+          }
+        }
+        console.log(board)
+        if ((key.row == 1 && key.block == 1) || (key.row == 3 && key.block == 3)) {
+          board[0].value++
+          board[4].value++
+          board[8].value++
+        }
+        if ((key.row == 1 && key.block == 3) || (key.row == 3 && key.block == 1)) {
+          board[2].value++
+          board[4].value++
+          board[6].value++
+        }
         !(function checkWin() {
           counter++;
           if (
@@ -128,7 +184,7 @@ function makegame() {
           } else {
             return;
           }
-          if (winner == true || winner == "draw") {
+          (function makewin() {
             console.log(board)
             console.log(counter)
             const body = document.querySelector("body");
@@ -140,30 +196,40 @@ function makegame() {
             const win = document.createElement("div");
             win.classList.add("winner");
             if (winner == "draw") {
-                win.textContent = "It's a draw";
-            } else if(player == "X"){
-                win.textContent = `${playerName} WINS`;
-            }
-            else{
-                win.textContent = `AI wins`
+              win.textContent = "It's a draw";
+            } else {
+              if (counter % 2 == 1) {
+                player = playerName
+              }
+              win.textContent = `${player} WINS`;
             }
             body.appendChild(win);
             body.appendChild(button);
             button.addEventListener("click", () => choosePlayer());
-          }
+          })()
         })();
+
         if (player == "X") {
           player = "O";
         } else player = "X";
-        if(counter % 2 == 1 && winner == false) {
-
-                let x = Math.floor(Math.random() * 9);
-                    while(board[x].takenBy != ""){
-                x = Math.floor(Math.random() * 9);
+        if (counter % 2 == 1 && winner == false) {
+          let biggestValue = 0
+          for (let x = 0; x <= 8; x++) {
+            if (board[x].takenBy == "") {
+              if (board[x].value > biggestValue) {
+                choice = board[x]
+                biggestValue = board[x].value
+              }
             }
-            console.log(x)
-            board[x].id.click()
           }
+          choice.id.click()
+          //          let x = Math.floor(Math.random() * 9);
+          //          while (board[x].takenBy != "") {
+          //            x = Math.floor(Math.random() * 9);
+          //          }
+          //
+          //          board[x].id.click()
+        }
       }
     });
   });
